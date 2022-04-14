19ba77eec3030ffee712de4cadd380066067981a
Martin Bachmann <martinpezbach@gmail.com>
2022-04-14 16:06:24 -0300

    Requisito 4 - getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,22 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId)) {
+    console.log('É manager');
+    return data.employees.reduce((acc, employee) => {
+      let returnVar = acc;
+      if (employee.managers.includes(managerId)) {
+        returnVar = [...acc, `${employee.firstName} ${employee.lastName}`];
+      }
+      return returnVar;
+    }, []);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
