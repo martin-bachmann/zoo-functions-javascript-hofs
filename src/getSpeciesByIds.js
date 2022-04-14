7e4cb611f2c4e16efaba2939f3d7661c56e3bb3e
Martin Bachmann <martinpezbach@gmail.com>
2022-04-14 15:02:53 -0300

    Requisito 1 - getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const animalsArr = ids.map((id) => species.find((animal) => animal.id === id));
+  return animalsArr;
 }
 
 module.exports = getSpeciesByIds;
