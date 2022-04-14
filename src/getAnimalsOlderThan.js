fbca2fe7839c6ad1f0fdc37095dc12e8c9cc2e8e
Martin Bachmann <martinpezbach@gmail.com>
2022-04-14 15:12:48 -0300

    Requisito 2 - getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animalObj = species.find((element) => element.name === animal); // Identifica a espécie correta de animal e retorna seu objeto
+  return animalObj.residents.every((resident) => resident.age >= age); // Verifica se todos os indivíduos da espécie são mais velhos que 'age'
 }
 
 module.exports = getAnimalsOlderThan;
