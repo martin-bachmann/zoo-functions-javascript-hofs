e3c84ce32e7a09079943df49a70099baef233042
Martin Bachmann <martinpezbach@gmail.com>
2022-04-14 15:23:15 -0300

    Requisito 3 - getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return employees.find(({ firstName, lastName }) => {
+    const verifier = (firstName === employeeName || lastName === employeeName);
+    return verifier;
+  });
 }
 
 module.exports = getEmployeeByName;
