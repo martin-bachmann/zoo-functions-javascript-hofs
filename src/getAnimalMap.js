61b665aeb738245ffcd7e8466c7ab6914152a4ac
Martin Bachmann <martinpezbach@gmail.com>
2022-04-14 22:41:03 -0300

    Requisito 7 - getAnimalMap [parcial]

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,43 @@
 const data = require('../data/zoo_data');
 
+function getAnimalMapComplex({ sorted, sex }) {
+  return data.species.reduce((acc, { location, name, residents }, index) => {
+    if (!acc[location]) { acc[location] = []; }
+    if (sex) {
+      const filterArr = residents.filter(({ sex: sexRes }) => sex === sexRes);
+      const nameArr = filterArr.map(({ name: nameRes }) => nameRes);
+      if (sorted) { nameArr.sort(); }
+      acc[location].push({ [name]: nameArr });
+    } else {
+      const nameArr = residents.map(({ name: nameRes }) => nameRes);
+      if (sorted) { nameArr.sort(); }
+      acc[location].push({ [name]: nameArr });
+    }
+    return acc;
+  }, {});
+}
+
+function getAnimalMapSimple() {
+  return data.species.reduce((acc, element) => {
+    if (!acc[element.location]) {
+      acc[element.location] = [];
+    }
+    acc[element.location].push(element.name);
+    return acc;
+  }, {});
+}
+
 function getAnimalMap(options) {
   // seu código aqui
+  if (!options) {
+    return getAnimalMapSimple();
+  }
+  if (!options.includeNames) {
+    return getAnimalMapSimple();
+  }
+  return getAnimalMapComplex(options);
 }
+
+getAnimalMap({ includeNames: true, sorted: true });
 
 module.exports = getAnimalMap;
