0572cce0da544cb2eaa25471447adbda7547202c
Martin Bachmann <martinpezbach@gmail.com>
2022-04-15 16:42:13 -0300

    Requisito 7 - getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,33 +1,39 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMapComplex({ sorted, sex }) {
-  return data.species.reduce((acc, { location, name, residents }, index) => {
+function getAnimalMapSex(sorted, sex, acc, location, name, residents) {
+  const filterArr = residents.filter(({ sex: sexRes }) => sex === sexRes);
+  const nameArr = filterArr.map(({ name: nameRes }) => nameRes);
+  if (sorted) { nameArr.sort(); }
+  acc[location].push({ [name]: nameArr });
+  return acc;
+}
+
+function getAnimalMapFull(sorted, acc, location, name, residents) {
+  const nameArr = residents.map(({ name: nameRes }) => nameRes);
+  if (sorted) { nameArr.sort(); }
+  acc[location].push({ [name]: nameArr });
+  return acc;
+}
+
+function getAnimalMapComplex({ sorted, sex }) { // Aqui novamente tive que quebrar em duas funções menores, pela complexidade no ESLint
+  return data.species.reduce((acc, { location, name, residents }) => {
     if (!acc[location]) { acc[location] = []; }
     if (sex) {
-      const filterArr = residents.filter(({ sex: sexRes }) => sex === sexRes);
-      const nameArr = filterArr.map(({ name: nameRes }) => nameRes);
-      if (sorted) { nameArr.sort(); }
-      acc[location].push({ [name]: nameArr });
-    } else {
-      const nameArr = residents.map(({ name: nameRes }) => nameRes);
-      if (sorted) { nameArr.sort(); }
-      acc[location].push({ [name]: nameArr });
+      return getAnimalMapSex(sorted, sex, acc, location, name, residents);
     }
-    return acc;
+    return getAnimalMapFull(sorted, acc, location, name, residents);
   }, {});
 }
 
 function getAnimalMapSimple() {
-  return data.species.reduce((acc, element) => {
-    if (!acc[element.location]) {
-      acc[element.location] = [];
-    }
-    acc[element.location].push(element.name);
+  return data.species.reduce((acc, { location, name }) => {
+    if (!acc[location]) { acc[location] = []; }
+    acc[location].push(name);
     return acc;
   }, {});
 }
 
-function getAnimalMap(options) {
+function getAnimalMap(options) { // Para não ter problemas com a complexidade da função no ESLint, dividi essa função em várias menores
   // seu código aqui
   if (!options) {
     return getAnimalMapSimple();
