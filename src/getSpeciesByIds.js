6c9702deb9aad1606e5ec991da4493d8d1a284aa
Martin Bachmann <martinpezbach@gmail.com>
2022-07-24 18:20:52 -0300

    Refatorações

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,8 +3,7 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  const animalsArr = ids.map((id) => species.find((animal) => animal.id === id));
-  return animalsArr;
+  return ids.map((id) => species.find((animal) => animal.id === id));
 }
 // Como o array de retorno sempre vai ter o mesmo tamanho que a entrada, o map funciona muito bem, com o find para encontrar a espécie
 // Como o map retorna um array, se a função não recebe nenhum parâmetro ele já retorna um array vazio
